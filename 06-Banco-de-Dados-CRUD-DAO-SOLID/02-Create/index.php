76440c5cecb4d5d7066f838999dadd5cc81a3e2c
neto <jsn.ads@gmail.com>
2021-04-05 22:05:32 -0300

    06-09 Concluido

--- 06-Banco-de-Dados-CRUD-DAO-SOLID/02-Create/index.php
+++ 06-Banco-de-Dados-CRUD-DAO-SOLID/02-Create/index.php
@@ -1,5 +1,12 @@
 <?php
     require 'config.php';
+
+    $lista = array();
+
+    $sql = $pdo->query("SELECT * FROM usuario");
+
+    ($sql->rowCount() > 0)? $lista = $sql->fetchAll(PDO::FETCH_ASSOC):""; 
+
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +27,18 @@
             <th>EMAIL</th>
             <th>ACOES</th>
         </tr>
+        <?php foreach($lista as $usuario):?>
+        <tr>
+            <th><?= $usuario['id'];?></th>
+            <th><?= $usuario['nome'];?></th>
+            <th><?= $usuario['email'];?></th>
+            <th>
+                <button><a href="editar.php?id=<?= $usuario['id'];?>" style="text-decoration: none;">Editar</a></button>
+                <button><a href="excluir.php?id=<?= $usuario['id']?>" style="text-decoration: none;">Excluir</a></button>
+            </th>
+        </tr>
+        <?php endforeach;?>
+        
     </table>
 
 </body>
