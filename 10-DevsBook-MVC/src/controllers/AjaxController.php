4bd890966a11e5f689d851e9671c7768da543510
Neto Evtek <jsn.ads@gmail.com>
2023-04-27 16:36:09 +0044

    Devsnote Upload de Fotos

--- 10-DevsBook-MVC/src/controllers/AjaxController.php
+++ 10-DevsBook-MVC/src/controllers/AjaxController.php
@@ -57,6 +57,80 @@ class AjaxController extends Controller
         echo json_encode($array);
         exit;
     }
+
+    public function upload()
+    {
+        $array = ['error' => ''];
+
+        if(isset($_FILES['photo']) && !empty($_FILES['photo']['tmp_name']))
+        {
+            $photo = $_FILES['photo'];
+
+            //padrao
+            $standardWidth = 800;
+            $standardHeight = 800;
+
+            if(in_array($photo['type'],['image/png','image/jpg','image/jpeg']))
+            {
+
+                //configuração de tamanho
+
+                list($origWidth,$origiHeight) = getimagesize($photo['tmp_name']);
+                $origRadio = $origWidth / $origiHeight;
+
+                $newWidth = $standardWidth;
+                $newHeight = $standardHeight;
+                $standardRadio = $standardWidth / $standardHeight;
+
+                if($standardRadio > $origRadio)
+                {
+                    $newWidth = $newHeight * $origRadio;
+                }
+                else
+                {
+                    $newHeight = $newWidth / $origRadio;
+                }
+
+                //cria o modelo
+
+                $standardImage = imagecreatetruecolor($newWidth, $newHeight);
+                switch($photo['type'])
+                {
+                    case 'image/png':
+                        $origiImage = imagecreatefrompng($photo['tmp_name']);
+                        break;
+                    case 'image/jpg':
+                    case 'image/jpeg':
+                        $origiImage = imagecreatefromjpeg($photo['tmp_name']);
+                        break;
+                }
+
+                //copia da imagem
+
+                imagecopyresampled(
+                    $standardImage, $origiImage,
+                    0,0,0,0,
+                    $newWidth, $newHeight, $origWidth, $origiHeight
+                );
+
+                //salvar 
+
+                $photoName = md5(time().rand(0,9999)).'.jpg';
+                imagejpeg($standardImage,'media/uploads/'.$photoName);
+
+                PostHelpers::addPost($this->loggedUser->id, 'photo', $photoName);
+
+            }
+
+        }else
+        {
+            $array['error'] = 'Nenhuma imagem enviada';
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode($array);
+        exit;
+    }
     
 }
 
