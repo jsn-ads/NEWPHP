6bf85d2197061d5562a65e147e41cb729a432438
Neto Home <jsn.ads@gmail.com>
2023-10-23 09:43:47 -0300

    devsbookPoo - Pagina Home

--- 11-DevsBook-POO/index.php
+++ 11-DevsBook-POO/index.php
@@ -11,7 +11,30 @@
 ?>
 
 <section class="feed mt-10">
-
+    <div class="row">
+        <div class="column pr-5">
+            <?php require 'partials/feed-editor.php';?>
+        </div>
+        <div class="column pl-5">
+            <div class="box banners">
+                <div class="box-header">
+                    <div class="box-header-text">Patrocinios</div>
+                    <div class="box-header-buttons">
+                        
+                    </div>
+                </div>
+                <div class="box-body">
+                    <a href=""><img src="<?=$base;?>/media/courses/laravel.png" /></a>
+                    <a href=""><img src="<?=$base;?>/media/courses/php.png" /></a>
+                </div>
+            </div>
+            <div class="box">
+                <div class="box-body m-10">
+                    Criado com ❤️ por B7Web
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 
