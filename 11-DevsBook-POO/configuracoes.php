57ee33063ba92a8386a0811553f24e2c700b969d
Neto Home <jsn.ads@gmail.com>
2025-01-02 17:14:00 -0300

    desvbookPoo - Configurações

--- 11-DevsBook-POO/configuracoes.php
+++ 11-DevsBook-POO/configuracoes.php
@@ -1,19 +1,24 @@
 <?php
-    require_once 'config.php';
-    require_once 'models/Auth.php';
-    require_once 'dao/UserDaoMysql.php';
+require_once 'config.php';
+require_once 'models/Auth.php';
+require_once 'dao/UserDaoMysql.php';
 
-    $auth = new Auth($pdo, $base);
-    $userInfo = $auth->checkToken();
-    $activeMenu = 'configuracoes';
+$auth = new Auth($pdo, $base);
+$userInfo = $auth->checkToken();
+$activeMenu = 'configuracoes';
 
-    require 'partials/header.php';
-    require 'partials/menu.php';
+require 'partials/header.php';
+require 'partials/menu.php';
 ?>
 
 <section class="feed mt-10">
-    
+
     <h1>Configurações</h1>
+
+    <?php if (!empty($_SESSION['flash'])): ?>
+        <?= $_SESSION['flash']; ?>
+        <?php $_SESSION['flash'] = '' ?>
+    <?php endif; ?>
 
     <form method="POST" class="config-form" enctype="multipart/form-data" action="configuracoes_action.php">
 
@@ -21,40 +26,40 @@
             Novo Avatar:
         </label>
         <input id="config.avatar" type="file" name="avatar">
-        <img class="mini-avatar" src="<?=$base;?>/media/avatars/<?=$userInfo->avatar;?>" alt="">
+        <img class="mini-avatar" src="<?= $base; ?>/media/avatars/<?= $userInfo->avatar; ?>" alt="">
 
         <label for="config.couver">
             Nova Capa:
         </label>
-        <input id="config.couver"type="file" name="cover">
-        <img class="mini-cover" src="<?=$base;?>/media/covers/<?=$userInfo->cover;?>" alt="">
+        <input id="config.couver" type="file" name="cover">
+        <img class="mini-cover" src="<?= $base; ?>/media/covers/<?= $userInfo->cover; ?>" alt="">
 
         <hr>
 
         <label for="config.name">
             Nome Completo:
         </label>
-        <input id="config.name" type="text" name="name" value="<?=$userInfo->name;?>">
+        <input id="config.name" type="text" name="name" value="<?= $userInfo->name; ?>">
 
         <label for="config.email">
             E-mail:
         </label>
-        <input id="config.email" type="e-mail" name="email" value="<?=$userInfo->email;?>">
+        <input id="config.email" type="e-mail" name="email" value="<?= $userInfo->email; ?>">
 
         <label for="config.birthdate">
             Data de Aniversario:
         </label>
-        <input id="config.birthdate" type="text" name="birthdate" value="<?= date('d/m/Y', strtotime($userInfo->birthdate));?>">
+        <input id="config.birthdate" type="text" name="birthdate" value="<?= date('d/m/Y', strtotime($userInfo->birthdate)); ?>">
 
         <label for="config.city">
             Cidade:
         </label>
-        <input id="config.city" type="text" name="city" value="<?=$userInfo->city;?>">
+        <input id="config.city" type="text" name="city" value="<?= $userInfo->city; ?>">
 
         <label for="config.work">
             Trabalho:
         </label>
-        <input id="config.work" type="text" name="work" value="<?=$userInfo->work;?>">
+        <input id="config.work" type="text" name="work" value="<?= $userInfo->work; ?>">
 
         <label for="config.password">
             Nova Senha:
@@ -67,21 +72,20 @@
         <input id="config.password_confirm" type="password" name="password_confirm">
 
         <button class="button">Salvar</button>
-    
+
     </form>
- 
+
 </section>
 
 <script src="https://unpkg.com/imask"></script>
 <script>
     IMask(
-        document.getElementById("config.birthdate"),
-        {
-            mask:'00/00/0000'
+        document.getElementById("config.birthdate"), {
+            mask: '00/00/0000'
         }
     )
 </script>
 
 <?php
-    require 'partials/footer.php';
+require 'partials/footer.php';
 ?>
--- 11-DevsBook-POO/configuracoes_action.php
+++ 11-DevsBook-POO/configuracoes_action.php
@@ -16,21 +16,56 @@ $work = filter_input(INPUT_POST, 'work');
 $password = filter_input(INPUT_POST, 'password');
 $password_confirm = filter_input(INPUT_POST, 'password_confirm');
 
+
 if ($name && $email) {
+
+    // E-MAIL
     if ($email != $userInfo->email) {
         if ($userDao->findByEmail($email) === $false) {
             $userInfo = $email;
         }
         $_SESSION['flash'] = 'E-mail ja esta sendo utilizado';
         header("Location:" . $base . "/configuracoes.php");
+        exit;
+    }
+
+    // BIRTHDATE
+    $birthdate = explode('/', $birthdate);
+    if (count($birthdate) != 3) {
+        $_SESSION['flash'] = 'Data de Aniversario invalida';
+        header("Location:" . $base . "/configuracoes.php");
+        exit;
+    }
+    $birthdate = $birthdate[2] . '-' . $birthdate[1] . '-' . $birthdate[0];
+    if (strtotime($birthdate) === false) {
+        $_SESSION['flash'] = 'Data de Aniversario invalida';
+        header("Location:" . $base . "/configuracoes.php");
+        exit;
+    }
+
+    //PASSWORD
+    if (!empty($password)) {
+        if ($password === $password_confirm) {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $userInfo->password = $hash;
+        } else {
+            $_SESSION['flash'] = 'Senhas estão diferentes';
+            header("Location:" . $base . "/configuracoes.php");
+            exit;
+        }
     }
 
     $userInfo->name = $name;
     $userInfo->$city = $city;
     $userInfo->work = $work;
+    $userInfo->birthdate = $birthdate;
 
-    return $userDao->update($userInfo);
+    $userDao->update($userInfo);
+
+    header("Location:" . $base . "/configuracoes.php");
+    exit;
 }
+
 
 $_SESSION['flash'] = 'Campo E-mail e/ou Nome esta vazio';
 header("Location:" . $base . "/configuracoes.php");
